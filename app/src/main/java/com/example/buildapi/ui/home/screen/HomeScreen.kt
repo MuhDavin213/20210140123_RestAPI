22193b2ea811f77b592edd1450551570d35beece
MuhDavin213 <davin.new58@gmail.com>
2023-12-27 23:07:26 +0700

    add icon button dihomescreen

--- app/src/main/java/com/example/buildapi/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/buildapi/ui/home/screen/HomeScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -96,6 +97,8 @@ fun KontakCard(
                     style = MaterialTheme.typography.titleLarge
                 )
                 Spacer(Modifier.weight(1f))
+                IconButton(onClick = {onDeleteClick(kontak)}) {
+                }
                 Icon(
                     imageVector = Icons.Default.Phone,
                     contentDescription = null,
@@ -125,7 +128,7 @@ fun KontakLayout(kontak: List<Kontak>, modifier: Modifier = Modifier){
         items(kontak){ kontak ->
             KontakCard(kontak = kontak, modifier = Modifier
                 .fillMaxWidth()
-                .clickable {  }
+                .clickable { }
             )
         }
         }
