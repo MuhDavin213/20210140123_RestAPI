9f94af11288748a49f057c762095ae2a8b9d93aa
MuhDavin213 <davin.new58@gmail.com>
2023-12-27 19:58:39 +0700

    add icon

--- app/src/main/java/com/example/buildapi/ui/KontakApp.kt
+++ app/src/main/java/com/example/buildapi/ui/KontakApp.kt
@@ -1,5 +1,8 @@
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
@@ -21,6 +24,10 @@ fun TopAppBarKontak(
         navigationIcon = {
             if (canNavigateBack){
                 IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = ""
+                    )
                 }
             }
         }
