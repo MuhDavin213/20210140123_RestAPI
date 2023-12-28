fe46fc3efe328d74a3cd884d0fce3885c8a2cfd0
MuhDavin213 <davin.new58@gmail.com>
2023-12-28 18:14:37 +0700

    add  Textfield nama

--- app/src/main/java/com/example/buildapi/ui/kontak/screen/InsertScreen.kt
+++ app/src/main/java/com/example/buildapi/ui/kontak/screen/InsertScreen.kt
@@ -1,9 +1,17 @@
 package com.example.buildapi.ui.kontak.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.buildapi.ui.kontak.viewmodel.InsertUiEvent
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormInputSiswa(
     insertUiEvent: InsertUiEvent,
@@ -11,5 +19,18 @@ fun FormInputSiswa(
     onValueChange: (InsertUiEvent) -> Unit = {},
     enabled: Boolean = true
 ){
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        OutlinedTextField(
+            value = insertUiEvent.nama,
+            onValueChange = {onValueChange(insertUiEvent.copy(nama = it)) },
+            label ={ Text(text = "Nama")},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true,
+            )
 
+}
 }
