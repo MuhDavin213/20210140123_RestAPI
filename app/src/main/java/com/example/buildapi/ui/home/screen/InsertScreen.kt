6e42f8aff3cc6339c4ede7815980aa28aa33d771
MuhDavin213 <davin.new58@gmail.com>
2023-12-28 15:16:40 +0700

    add  sealed class dan function HomeScreen

--- app/src/main/java/com/example/buildapi/ui/home/screen/InsertScreen.kt
+++ app/src/main/java/com/example/buildapi/ui/home/screen/InsertScreen.kt
@@ -1,22 +1,89 @@
 package com.example.buildapi.ui.home.screen
 
+import TopAppBarKontak
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.buildapi.R
 import com.example.buildapi.model.Kontak
+import com.example.buildapi.navigation.DestinasiNavigasi
+import com.example.buildapi.ui.PenyediaViewModel
+import com.example.buildapi.ui.home.viewmodel.HomeViewModel
 
+object DestinasiHome: DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = "Kontak"
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Int)  ->Unit = {},
+    viewModel: HomeViewModel = viewModel (factory = PenyediaViewModel.Factory)
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBarKontak(
+                title = DestinasiHome.titleRes,
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior,
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "add Kontak"
+                )
+            }
+        },
+    ){ innerPadding ->
+        
+        HomeStatus(
+            kontakUIState = viewModel.kontakUIState,
+            retryAction = {
+                          viewModel.getKontak()
+            },
+            modifier = Modifier.padding(innerPadding),
+            onDetailClick = onDetailClick,
+            onDeleteClick = {
+                viewModel.deleteKontak(it.id)
+                viewModel.getKontak()
+            }
+            )
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
